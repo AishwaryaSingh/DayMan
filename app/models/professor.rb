8a8b78e3897eb0fe36a6687acb4c053c1685adae
AishwaryaSingh <1994aishwaryasingh@gmail.com>
2015-03-17 17:05:53 +0530

    Reinitializing

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,10 +1,3 @@
 class Professor < ActiveRecord::Base
 
-	has_many :subjects
-	has_many :batches
-	has_many :subBatches
-	has_many :lectures
-
-	belongs_to :branch
-
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,11 +1,3 @@
 class Subject < ActiveRecord::Base
 
-	has_many :professors
-	has_many :lectures
-	
-	belongs_to :branch
-	belongs_to :batch
-	belongs_to :subBatch
-	belongs_to :semester
-
 end
