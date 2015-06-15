2b0078b3908b6b3a02ed47630a056c029ee311e1
AishwaryaSingh <1994aishwaryasingh@gmail.com>
2015-06-15 16:33:57 +0530

    trying to deploy v2

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150504151205) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "batches", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
